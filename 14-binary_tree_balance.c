6e5ff7368c69d3f23b8275867360a67772aa912c
Ogollah <kogollah2011@gmail.com>
2023-08-30 19:51:13 +0300

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,3 +21,20 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (height_left - height_right);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Tree to measure the height of.
+ *
+ * Return: Height of the tree, 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height + 1 : right_height + 1));
+}
