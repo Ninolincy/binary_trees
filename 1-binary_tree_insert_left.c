fa537eefab7d3f5cd9a718fdf843168a6308148d
Ogollah <kogollah2011@gmail.com>
2023-08-31 22:56:39 +0300

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,28 +6,26 @@
  * Description: If parent  has a left child, the new node takes its place
  * and the old is set as left child to the new node.
  * Return: pointer to the created node, NULL on failure or parent is NULL
-*/
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	{
-	if (new_node == NULL)
+	if (!parent)
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = parent->left;
 	new_node->right = NULL;
-
-	if (parent->left != NULL)
-		parent->left->parent = new_node;
+	new_node->left = parent->left;
 	parent->left = new_node;
+
+	if (new_node->left)
+		new_node->left->parent = new_node;
 
 	return (new_node);
 }
-
