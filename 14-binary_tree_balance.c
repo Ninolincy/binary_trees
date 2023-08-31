c3c1a8a442fbf5bf5068ac2b5339c56a7fc1d4ea
Ogollah <kogollah2011@gmail.com>
2023-08-31 22:43:54 +0300

    Tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,12 +30,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left = 0;
+	size_t height_right = 0;
+
 	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ?
-		 left_height + 1 : right_height + 1));
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_left > height_right ? height_left : height_right);
 }
